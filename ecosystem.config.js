181d55219f4e90709dc87482ebaf03a93b8f7c65
hussain-zz-nasr <h.work.ahmed@gmail.com>
2025-08-02 09:26:20 +0300

    a

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'react-app',
+      script: 'npm',
+      args: 'start',
+      interpreter: 'cmd.exe',
+      cwd: 'C:/Users/Administrator/Documents/deployfile/frontend/y'
+    }
+  ]
+};
